621bd328fd6d877ec771f00c9e3a253a0399c3fa
Yi Nam <nyn623@naver.com>
2022-05-19 01:13:12 +0900

    SearchResult
    SearchResult 검색결과 페이지 추가

---
+++ vrisfront/src/pages/SearchResult.js
@@ -0,0 +1,16 @@
+import Header from '../components/common/Header';
+import MainMember from '../../src/components/Main/MainMember';
+import { Flex, Image, Input,Text, IconButton,Button } from '@chakra-ui/react';
+import React from 'react';
+import MainSearch from '../../src/components/Main/MainSearch';
+
+const SearchResult = () => {
+  return (
+    <>
+      <Header />
+      <MainMember />
+    </>
+  );
+};
+
+export default SearchResult;
