e6901b00a8c4afc2374db636246fad00e0d5c1be
Yi Nam <nyn623@naver.com>
2022-05-19 02:11:58 +0900

    검색결과 페이지 컴포들 추가
    SearchResult (메인 js)
    
    Card.js
    CardComponent.js
    Data.js
    이렇게 3개 Search 폴더 추가

--- vrisfront/src/pages/SearchResult.js
+++ vrisfront/src/pages/SearchResult.js
@@ -3,11 +3,16 @@ import MainMember from '../../src/components/Main/MainMember';
 import { Flex, Image, Input,Text, IconButton,Button } from '@chakra-ui/react';
 import React from 'react';
 import MainSearch from '../../src/components/Main/MainSearch';
+import CardComponent from '../../src/components/Search/CardComponent';
+
 
 const SearchResult = () => {
   return (
     <>
       <Header />
+
+
+      
       <MainMember />
     </>
   );
