5a3ad6a9aad1d5f081b89928a07bac415dfc1275
Iji Shin <hee981226@naver.com>
2022-05-29 00:21:11 +0900

    Update MyPage.js

--- vrisfront/src/pages/MyPage.js
+++ vrisfront/src/pages/MyPage.js
@@ -1,38 +1,9 @@
-import {
-    IconButton,
-    Input,
-    InputRightElement,
-    InputLeftElement,
-    FormControl,
-    Box,
-    useDisclosure,
-    InputGroup,
-    useToast
-  } from "@chakra-ui/react";
-import {
-    Text,
-    Image,
-    Button,
-    ChakraProvider,
-    Flex,
-    Spacer,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-  } from '@chakra-ui/react'
 import React from 'react';
 import Header from '../components/common/Header';
-import { IoIosOptions } from "react-icons/io";
 
 
 
 const MyPage = () => {
-
-    const { isOpen, onOpen, onClose } = useDisclosure();
     
     return (
         <>
