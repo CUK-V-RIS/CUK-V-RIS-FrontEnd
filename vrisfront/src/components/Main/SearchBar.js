5f750d7dd72a829649881df3751c359258074c8c
Iji Shin <hee981226@naver.com>
2022-05-28 16:31:32 +0900

    서치바 코드구조, 디자인 변경

--- vrisfront/src/components/Main/SearchBar.js
+++ vrisfront/src/components/Main/SearchBar.js
@@ -24,8 +24,6 @@ import {
 import React from "react";
 import { BiListCheck, BiSearchAlt2 } from "react-icons/bi";
 import { IoIosOptions } from "react-icons/io";
-import styled from 'styled-components';
-import "./SearchBar.css"
 import { Link } from 'react-router-dom';
 import SearchResult from "../../pages/SearchResult";
 import axios from 'axios'; //result 결과 연결
@@ -40,27 +38,40 @@ function SerarchBar({ onClick, onChange }) {
   return (
 
     <>
-      <div className="input-group">
-        <input id="input1"
-          type="search"
-          className="form-control rounded"
-          placeholder="식당 이름을 입력해 주세요"
+      <Box mx="auto">
+        <FormControl display="flex" h="100px">
+          <InputGroup>
+          <Link to='/SearchResult'>
+            <InputLeftElement mx="10px" h="50px">
+                <IconButton
+                  variant="filled"
+                color="green"
+                icon={<BiSearchAlt2 />}
+                />
+              </InputLeftElement>
+              </Link>  
+            <Input
+              id="input1"
+              type="search"
+              bgcolor="white"
+              placeholder="    식당 이름을 입력해 주세요"
+              px="10px"
+              h="50px"
+              width="500px"
+              borderRadius="100"
           onChange={onChange}
-        />
-        <Link to='/SearchResult'>
-          <button type="button" id="search_btn" onClick={onClick}>
-            <BiSearchAlt2 className="search_icon"></BiSearchAlt2>
-          </button>
-        </Link>
-        <Link to='/mypage'>
-          <button type="button" id="filter_btn" onClick={onClick}>
-            <IoIosOptions className="filter_icon"></IoIosOptions>
-        </button>
-        </Link>
-      </div>
+            />
 
-      <ChakraProvider>
-      <Button onClick={onOpen}>모달 테스트</Button>
+            <InputRightElement>
+            <ChakraProvider>
+                <IconButton icon={<IoIosOptions size="25px" />}
+                  onClick={onOpen}
+                  h="50px"
+                  variant="ghost"
+                  color="green"
+                  mr="10px"
+                >
+                </IconButton>
         <Modal onClose={onClose} isOpen={isOpen} size="xl"
         scrollBehavior="inside" colorScheme="Green"
       isCentered>
@@ -80,11 +91,18 @@ function SerarchBar({ onClick, onChange }) {
                 size="md"
                 mt="5rem"
                 alignSelf="flex-end"
-                onClick={onClose}>닫기</Button>
+                onClick={onClose}>선택 완료</Button>
             </ModalFooter>
           </ModalContent>
         </Modal>
         </ChakraProvider>
+            </InputRightElement>
+
+          </InputGroup>
+        </FormControl>
+
+      </Box>
+
     </>
   );
 
